08b904f1db6b0f9a924425bef565df5affa0b158
ELird <edgar.Lird@gmail.com>
2019-04-28 23:23:49 -0400

    se agrego la gema rails_admin para Panel de Administracion

--- carpinteria/config/routes.rb
+++ carpinteria/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   #rutas para customer
   get 'customer/index'
   get 'customer/new'
--- carpinteria/db/schema.rb
+++ carpinteria/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_191530) do
+ActiveRecord::Schema.define(version: 2019_04_28_221346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,18 @@ ActiveRecord::Schema.define(version: 2019_04_28_191530) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["employee_id"], name: "index_salaries_on_employee_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "utilities", force: :cascade do |t|
