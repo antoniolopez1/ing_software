415a18269847391e85e50bee63022baca0fa87be
ELird <edgar.Lird@gmail.com>
2019-04-28 23:41:27 -0400

    Se agrega gema devise

--- carpinteria/db/schema.rb
+++ carpinteria/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_221346) do
+ActiveRecord::Schema.define(version: 2019_04_29_032944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
