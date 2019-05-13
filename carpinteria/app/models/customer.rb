c331a05403174d15b1912075b5214cb8fc8f62b5
magalysuchecki <suchecki.magaly24@gmail.com>
2019-05-12 22:16:43 -0400

    validaciones 1.1

--- carpinteria/app/models/customer.rb
+++ carpinteria/app/models/customer.rb
@@ -5,14 +5,14 @@ class Customer < ApplicationRecord
 validates :name,  	  	  :presence => {:message => "Campo obligatorio"},
 						  format: { with: /\A[a-zA-Z]+\z/, message: "Solo letras" }
 validates :lastname,  	  :presence => {:message => "Campo obligatorio"},
-						  format: { with: /\A[a-zA-Z]+\z/, message: "Solo letras" }
-validates :address,  	  :presence => {:message => "Campo obligatorio"} 
+						  format: { with: /\A([a-zA-Z]|[a-zA-Z][\. ])+\z/, message: "Solo letras" }
+validates :address,  	  :presence => {:message => "Campo obligatorio"}
 validates :telephone,  	  :presence => {:message => "Campo obligatorio"},
 						  numericality: { only_integer: true }
-validates :document,  	  :presence => {:message => "Campo obligatorio"}, 
+validates :document,  	  :presence => {:message => "Campo obligatorio"},
 						  :uniqueness => {:message => "El valor %{value} ya existe"},
 						  numericality: { only_integer: true }
-validates :ruc, 		  numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 9  }	  	  
+validates :ruc, 		  numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 9  }
 
 
 end
