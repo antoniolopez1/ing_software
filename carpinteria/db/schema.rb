9b47dc277ed6db82b9e84859cd67a58f354db0cd
USUARIO <USUARIO@EMAIL.com>
2019-06-19 19:28:52 -0400

    fixtures v1.0

--- carpinteria/db/schema.rb
+++ carpinteria/db/schema.rb
@@ -212,7 +212,6 @@ ActiveRecord::Schema.define(version: 2019_05_26_205437) do
     t.index ["utility_id"], name: "index_u_purchase_details_on_utility_id"
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -224,7 +223,6 @@ ActiveRecord::Schema.define(version: 2019_05_26_205437) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   create_table "utilities", force: :cascade do |t|
     t.bigint "utilities_type_id"
